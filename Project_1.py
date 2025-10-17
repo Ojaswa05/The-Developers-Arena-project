4b47eff3f8658c9e5107535d840105f5b5be0a23
Ojaswa Srivastava <ojaswasrivastava05192876@gmail.com>
2025-10-17 23:42:23 +0530

    Add files via upload

---
+++ Project_1.py
@@ -0,0 +1,8 @@
+name = input("What's your name?")
+age = input("How old are you?")
+hobby = input("What is your favorite hobby?")
+
+print("\n--Personal Introduction--")
+print(f"Hello {name}!, Nice to meet you.")
+print(f"You are {age} years old and love {hobby}.")
+print("That’s awesome! Hope you have a great day ahead!")
